6fce504ce6294e7795105d51c3f33099180309bf
ella.krelovich <ella.krelovich@MU-C1-11.mullen.pvt.k12.co.us>
2023-02-10 11:07:21 -0700

    Finished Player pojo

--- Train Game/src/Player.java
+++ Train Game/src/Player.java
@@ -1,4 +1,46 @@
 
 public class Player {
+	
+	protected String name;
+	protected int money;
+	protected int location;
+	
+	public Player(String n, int m, int l)
+	{
+		name = n;
+		money = m;
+		location = l;
+	}
+
+	public String getName()
+		{
+			return name;
+		}
+
+	public void setName(String name)
+		{
+			this.name = name;
+		}
+
+	public int getMoney()
+		{
+			return money;
+		}
+
+	public void setMoney(int money)
+		{
+			this.money = money;
+		}
+
+	public int getLocation()
+		{
+			return location;
+		}
+
+	public void setLocation(int location)
+		{
+			this.location = location;
+		}
+	
 
 }

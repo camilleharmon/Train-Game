2c34e4210847e7b7b56d88e315f6b165fe81511a
ellakrelo <106175735+ellakrelo@users.noreply.github.com>
2023-02-13 20:37:32 -0700

    Added getters and setters

--- Train Game/src/Character.java
+++ Train Game/src/Character.java
@@ -13,4 +13,41 @@ public class Character {
 		location = l;
 		bond = b;
 	}
-}////
+
+        public String getName()
+		{
+			return name;
+		}
+
+	public void setName(String name)
+		{
+			this.name = name;
+		}
+        public String getSecondName()
+		{
+			return secondName;
+		}
+
+	public void setSecondName(String secondName)
+		{
+			this.secondName = secondName;
+		}
+        public int getLocation()
+		{
+			return location;
+		}
+
+	public void setLocation(int location)
+		{
+			this.location = location;
+		}
+        public int getBond()
+		{
+			return bond;
+		}
+
+	public void setBond(int bond)
+		{
+			this.bond = bond;
+		}
+}
